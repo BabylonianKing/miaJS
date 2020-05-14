7ddbb927f5500d6bbfe1fb2ba434aabc30bdc7a1
azarl <52580397+lucas-azar@users.noreply.github.com>
2020-05-14 16:36:27 -0400

    Resolved issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ConvoDashboardComponent } from './convo-dashboard/convo-dashboard.component';
-<<<<<<< HEAD
-import { LoginComponent } from './login/login.component';
-=======
-import { MiaComponent } from './mia/mia.component';
 import { LoginComponent } from './login/login.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -18,7 +14,6 @@ import { environment } from '../environments/environment';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent},
-<<<<<<< HEAD
   { path: 'chat', component: ConvoDashboardComponent},
   { path: 'login', component: LoginComponent}
 =======
