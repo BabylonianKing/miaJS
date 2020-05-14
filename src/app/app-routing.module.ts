6727fd303aa8e3533cce8a581b65ce355a9b97bc
azarl <52580397+lucas-azar@users.noreply.github.com>
2020-05-14 16:36:58 -0400

    Resolved some more issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,19 +8,10 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 
-
->>>>>>> 6bec45a2799aa6a9e4fc6b45f90c3a1f1d9c5648
-
-
 const routes: Routes = [
   { path: '', component: MainPageComponent},
   { path: 'chat', component: ConvoDashboardComponent},
   { path: 'login', component: LoginComponent}
-=======
-  { path: 'convo-dashboard', component: ConvoDashboardComponent},
-  { path: 'mia', component: MiaComponent},
-  { path: 'login', component: LoginComponent},
->>>>>>> 6bec45a2799aa6a9e4fc6b45f90c3a1f1d9c5648
 ];
 
 @NgModule({
