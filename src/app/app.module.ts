d17c49a60772677d86a3997cf368cea18daa7d49
azarl <52580397+lucas-azar@users.noreply.github.com>
2020-05-14 16:41:22 -0400

    Issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,14 @@ import { ConvoDashboardComponent } from './convo-dashboard/convo-dashboard.compo
 import { MiaComponent } from './mia/mia.component';
 import { ChatMessageComponent } from './mia/chat-message/chat-message.component';
 import { ChatFormComponent } from './mia/chat-form/chat-form.component';
-<<<<<<< HEAD
 import { ConvosComponent } from './convos/convos.component';
 import { ConvoCardComponent } from './convos/convo-card/convo-card.component';
-=======
->>>>>>> 6bec45a2799aa6a9e4fc6b45f90c3a1f1d9c5648
 import { LoginComponent } from './login/login.component';
 
 /* Firebase services */
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { environment } from '../environments/environment';
-<<<<<<< HEAD
 import { AngularFirestoreModule } from '@angular/fire/firestore'
 
 /* Auth service */
@@ -29,11 +25,6 @@ import { AuthenticationService } from '../shared/services/authentication.service
 
 /* DB CRUD service */
 import { FirebaseService } from '../shared/services/firebase.service';
-=======
-
-/* Auth service */
-import { AuthenticationService } from '../shared/services/authentication.service';
->>>>>>> 6bec45a2799aa6a9e4fc6b45f90c3a1f1d9c5648
 
 @NgModule({
   declarations: [
@@ -43,13 +34,9 @@ import { AuthenticationService } from '../shared/services/authentication.service
     MiaComponent,
     ChatMessageComponent,
     ChatFormComponent,
-<<<<<<< HEAD
     ConvosComponent,
     ConvoCardComponent,
     LoginComponent
-=======
-    LoginComponent,
->>>>>>> 6bec45a2799aa6a9e4fc6b45f90c3a1f1d9c5648
   ],
   imports: [
     BrowserModule,
@@ -58,10 +45,7 @@ import { AuthenticationService } from '../shared/services/authentication.service
     BrowserAnimationsModule,
     FormsModule,
     AngularFireAuthModule,
-<<<<<<< HEAD
     AngularFirestoreModule,
-=======
->>>>>>> 6bec45a2799aa6a9e4fc6b45f90c3a1f1d9c5648
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [AuthenticationService],
