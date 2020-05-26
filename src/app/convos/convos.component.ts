fe7e4f0279e8dc1a42274ffbbdc6e57da26bc219
Gongsta <gong.steven@hotmail.com>
2020-05-25 20:34:34 -0400

    job cards

--- src/app/convos/convos.component.ts
+++ src/app/convos/convos.component.ts
@@ -1,8 +1,22 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FirebaseService } from 'src/shared/services/firebase.service';
-import { AuthenticationService } from 'src/shared/services/authentication.service';
-import { CookieService } from 'ngx-cookie-service';
-import { AngularFirestore } from '@angular/fire/firestore';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import {
+  FirebaseService
+} from 'src/shared/services/firebase.service';
+import {
+  AuthenticationService
+} from 'src/shared/services/authentication.service';
+import {
+  CookieService
+} from 'ngx-cookie-service';
+import {
+  AngularFirestore
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'convos',
@@ -11,9 +25,9 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class ConvosComponent implements OnInit {
 
-  items: Array<any>;
-  searchValue: string="";
-  org_filtered_items: Array<any>;
+  items: Array < any > ;
+  searchValue: string = "";
+  org_filtered_items: Array < any > ;
 
   @Input() isSelected: boolean = false;
   @Output() selectedUser = new EventEmitter();
@@ -23,7 +37,7 @@ export class ConvosComponent implements OnInit {
     private afAuth: AuthenticationService,
     private cookie: CookieService,
     public db: AngularFirestore
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     // Load conversations
@@ -33,26 +47,46 @@ export class ConvosComponent implements OnInit {
     //Fetching each document in collection, loading all users as cards, including the user itself.
     let databaseOfUsers = []
 
+
+
     let usersRef = this.db.collection("conversation-cards").get().toPromise()
-    .then(snapshot => {
-      if (snapshot.empty) {
-        console.log("User databse is empty");
-        return
-      } 
-      snapshot.forEach(doc => {
-        databaseOfUsers.push({id: doc.id, 
-          jobTitle: doc.data().displayName, 
-          // organization: "test Organization",
-          // lastMessage: "Hello",
-          // isRead: true
+      .then(snapshot => {
+        if (snapshot.empty) {
+          console.log("User databse is empty");
+          return
+        }
+        snapshot.forEach(doc => {
+          
+          let orgId = doc.data().orgId
+          let orgData;
+          try {
+            console.log(orgId)
+            this.db.collection("organizations").doc(orgId).get().toPromise().then(document => {
+              orgData = document.data()
+              console.log(document.data())
+            }).then(random => {
+              databaseOfUsers.push({
+                //id will be the job id
+                id: doc.id,
+                jobTitle: doc.data().jobTitle,
+                organization: orgData.name,
+                location: orgData.location
+                // lastMessage: "Hello",
+                // isRead: true
+              })
+            }
+            )
+          } catch (error) {
+            console.log(`An error occurred, ${error}`)
+          }
+
         })
+        //Updating the items with the rry
+        this.items = databaseOfUsers
+
+
       })
-      //Updating the items with the rry
-      this.items = databaseOfUsers
 
-
-    })
-    
     //To continue implementing, reading from conversation-cards, each person is a document, storing objects of people they talked to, including time element, most recent text, 
     //const user2Id = JSON.parse(localStorage.getItem('currentTexter')).uid;
 
@@ -77,28 +111,28 @@ export class ConvosComponent implements OnInit {
     //   console.log(this.items)
     // })
   }
-  
+
   // Don't use firebase service everytime (javascript filter)
   // Subscribe only to uid, then use js filter on items array
-  
+
   searchByOrg() {
     let value = this.searchValue.toLowerCase();
     this.firebaseService.searchOrganization(value)
-    .subscribe(result => {
-      this.org_filtered_items = result;
-      this.items = this.org_filtered_items;
-    });
-    }
+      .subscribe(result => {
+        this.org_filtered_items = result;
+        this.items = this.org_filtered_items;
+      });
+  }
 
-    changeChat(item) {
+  changeChat(item) {
 
-      //Ensuring that only one convo-card is highlighted at a time
-      this.items.forEach(function(part, index) {
-        this[index].isSelected = false;
-      }, this.items)
-      this.selectedUser.emit(item.id);
-      item.isSelected = !item.isSelected;
+    //Ensuring that only one convo-card is highlighted at a time
+    this.items.forEach(function (part, index) {
+      this[index].isSelected = false;
+    }, this.items)
+    this.selectedUser.emit(item.id);
+    item.isSelected = !item.isSelected;
 
-    }
+  }
 
 }
