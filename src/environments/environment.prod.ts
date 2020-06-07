6664e63375e85ebe9c494d0ee3bc9e53eb441eab
Gongsta <gong.steven@hotmail.com>
2020-06-07 16:38:53 -0400

    updating enviornment ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDyVvcRhCI_0iH6MMBkNFPXCgzLSnwFnKM",
+    authDomain: "mia-test-sgwxam.firebaseapp.com",
+    databaseURL: "https://mia-test-sgwxam.firebaseio.com",
+    projectId: "mia-test-sgwxam",
+    storageBucket: "mia-test-sgwxam.appspot.com",
+    messagingSenderId: "211114227453",
+    appId: "1:211114227453:web:54ec075371fffe26abf094"
+  }
 };
