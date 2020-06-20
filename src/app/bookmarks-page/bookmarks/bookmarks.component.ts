902ec4ff50c5d0f2010c04a2079d137fddbd9eb7
Gongsta <gong.steven@hotmail.com>
2020-06-20 12:03:10 -0400

    bookmarks mvp

--- src/app/bookmarks-page/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks-page/bookmarks/bookmarks.component.ts
@@ -26,7 +26,7 @@ import {
   styleUrls: ['./bookmarks.component.scss']
 })
 export class BookmarksComponent implements OnInit {
-  items: Array < any > ;
+  items: Array < any > = [];
   userId = JSON.parse(localStorage.getItem('user')).uid;
   jobTitle;
   jobId;
@@ -44,7 +44,7 @@ export class BookmarksComponent implements OnInit {
   jobType;
   requirements;
   applicationURL;
-
+  filteredJobs: any = null;
 
   constructor(public firebaseService: FirebaseService,
     private afAuth: AuthenticationService,
@@ -54,154 +54,36 @@ export class BookmarksComponent implements OnInit {
 
   ngOnInit(): void {
 
-    let databaseOfUsers = []
-    let filteredJobs = []
+    //Load bookmarks on init
+    let bookmarksRef = this.db.collection("bookmarks").doc(this.userId).collection("bookmarks").get().toPromise()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        this.items.push(doc.data())
+        console.log(this.items)
 
-
-    let usersRef = this.db.collection("jobs").get().toPromise()
-      .then(snapshot => {
-        if (snapshot.empty) {
-          console.log("User databse is empty");
-          return
-        }
-
-        snapshot.forEach(doc => {
-          let orgId = doc.data().orgId
-
-          if (orgId === undefined) {
-
-            let jobId = doc.id
-            let jobTitle = doc.data().jobTitle
-            let organization = doc.data().orgName
-            let location = doc.data().location
-           let lowSalary = doc.data().salary
-           let highSalary = doc.data().salary
-            let jobDescription = doc.data().jobDescription
-            let responsabilities = doc.data().responsabilities
-            let jobType = doc.data().jobType
-            let requirements = doc.data().requirements
-            let applicationURL = doc.data().applicationURL
-            let imageURL = "https://uploads-ssl.webflow.com/5ea1997894e4390e5fbe12b2/5ea3164c953e8a56201c055c_icons8-target-50.png"
-
-            // console.log(doc.data().orgName)
-            databaseOfUsers.push({
-    
-              jobId: jobId,
-              jobTitle: jobTitle,
-              organization: organization,
-              location: location,
-              lowSalary: lowSalary,
-              highSalary: highSalary,
-              jobDescription: jobDescription,
-              responsabilities: responsabilities,
-              jobType: jobType,
-              requirements: requirements,
-              applicationURL: applicationURL,
-              imageURL: imageURL
-    
-            })
-
-
-
-
-
-
-          } else {
-
-
-            //Reset the image url to null  
-            this.afStorage.ref(`/orgImages/${orgId}`).getDownloadURL().toPromise().then(data => {
-
-
-              //If there isn't an image, use the webflow image
-
-              if (!this.imageURL) {
-                this.imageURL = "https://uploads-ssl.webflow.com/5ea1997894e4390e5fbe12b2/5ea3164c953e8a56201c055c_icons8-target-50.png"
-              }
-
-
-                let orgData;
-                try {
-                  this.db.collection("organizations").doc(orgId).get().toPromise().then(document => {
-                    orgData = document.data()
-                  }).then(random => {
-                    this.jobId = doc.id
-                    this.jobTitle = doc.data().jobTitle
-                    this.organization = orgData.name
-                    this.location = orgData.location
-                    this.lowSalary = doc.data().salary.low
-                    this.highSalary = doc.data().salary.high
-                    this.jobDescription = doc.data().jobDescription
-                    this.responsabilities = doc.data().responsabilities
-                    this.jobType = doc.data().jobType
-                    this.requirements = doc.data().requirements
-                    this.applicationURL = doc.data().applicationURL
-
-
-
-                    // this.db.collection("conversations").doc(this.userId).collection(doc.data().jobId).   get().toPromise()
-                    // .then()
-                    //Putting it here to ensure that image loads properly
-                    this.imageURL = data;
-
-
-
-                    databaseOfUsers.push({
-                      //id will be the job id
-                      jobId: this.jobId,
-                      jobTitle: this.jobTitle,
-                      organization: this.organization,
-                      location: this.location,
-                      imageURL: this.imageURL,
-                      orgId: orgId,
-                      lowSalary: this.lowSalary,
-                      highSalary: this.highSalary,
-                      responsabilities: this.responsabilities,
-                      jobType: this.jobType,
-                      requirements: this.requirements,
-                      applicationURL: this.applicationURL,
-                      jobDescription: this.jobDescription
-
-                      // lastMessage: this.lastMessage,
-                      // isRead: true
-                    })
-
-
-                  })
-                } catch (error) {
-                  console.log(`An error occurred, ${error}`)
-                }
-
-                
-
-              
-
-
-              this.imageURL = null
-
-            })
-
-            
-
-    
-    
-          }
-
-
-
-        })
-        //Updating the items with the rry
-        this.items = databaseOfUsers
 
       
 
 
       })
 
+    })
+
+
+
+  //Loading bookmark changes on the database
+  let query = this.db.collection("bookmarks").doc(this.userId).collection("bookmarks")
+  query.valueChanges().subscribe(data => {
+    // this.messages = [];
+    // this.messages.push(data.values);
+    this.items = data
+    // this.db.collection("conversation-cards").doc(this.userId).collection(this.currentTexterId).set(data[-1]
+
+  })
+
 
   }
 
-  filteredJobs: any = null;
 
   searchByOrg() {
     const value = this.searchValue.toLowerCase();
--- src/app/mia/chat-message/chat-message.component.ts
+++ src/app/mia/chat-message/chat-message.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Chat } from 'src/shared/models/chat.model';
 import { Router} from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 @Component({
   selector: 'chat-message',
   templateUrl: './chat-message.component.html',
   styleUrls: ['./chat-message.component.scss']
 })
+
 export class ChatMessageComponent implements OnInit {
 
 // Content 
@@ -23,21 +27,31 @@ export class ChatMessageComponent implements OnInit {
 @Input() jobSubtitle: boolean;
 
 @Output() learnMoreDescriptionEmitter = new EventEmitter<string>(); 
+userId = JSON.parse(localStorage.getItem('user')).uid;
 
 
 
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    public afStorage: AngularFireStorage,
+    public db: AngularFirestore,
+    ) { }
 
   ngOnInit(): void {
   }
 
-  openExternalPage() {
-    window.open('https://www.facebook.com/', "_blank")
-  }
-
   bookmarkJob() {
     console.log("Job bookmarked")
+    let data = {
+      learnMoreDescription: this.learnMoreDescription,
+      applyNowUrl: this.applyNowUrl,
+      imageUrl: this.imageUrl,
+      // jobImageURL: this.jobImageURL,
+      jobTitle: this.jobTitle,
+      jobSubtitle: this.jobSubtitle,    }
+
+    let ref = this.db.collection("bookmarks").doc(this.userId).collection("bookmarks")
+    ref.add(data);
   }
   learnMore() {
     this.learnMoreDescriptionEmitter.emit(this.learnMoreDescription)
