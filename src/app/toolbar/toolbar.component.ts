6015f19e09c4e360268d3dfeb5619e716dac0609
Gongsta <gong.steven@hotmail.com>
2020-06-20 12:36:10 -0400

    message and toolbar

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/shared/services/authentication.service';
 import { MenuToggleService } from 'src/shared/services/menu-toggle.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'toolbar',
@@ -11,9 +12,21 @@ export class ToolbarComponent implements OnInit {
 
   constructor(
     public afAuth: AuthenticationService,
-    public sideNavService: MenuToggleService) { }
+    public sideNavService: MenuToggleService,
+    public db: AngularFirestore) { }
+
+    bookmarksCount;
+    userId = JSON.parse(localStorage.getItem('user')).uid;
 
   ngOnInit(): void {
+
+      //Loading bookmark changes on the database
+  let query = this.db.collection("bookmarks").doc(this.userId).collection("bookmarks")
+  query.valueChanges().subscribe(data => {
+
+    this.bookmarksCount = data.length
+
+  })
   }
 
   signOut() {
