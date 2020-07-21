a1728160de0bb31d4bfe7c8a6e34d280e0b5f64b
azarl <52580397+lucas-azar@users.noreply.github.com>
2020-07-21 13:24:46 -0400

    Fix your shit Steven

--- src/app/mia/chat-message/chat-message.component.ts
+++ src/app/mia/chat-message/chat-message.component.ts
@@ -15,10 +15,27 @@ export class ChatMessageComponent implements OnInit {
 
 // Content
 @Input() message: string;
-@Input() richCard: boolean;
 @Input() timestamp: string;
-@Input() cards: string;
+
+
+@Input() learnMoreDescription: string;
+@Input() applyNowUrl: string;
+@Input() imageUrl: string;
 @Input() reply: boolean;
+@Input() jobImageURL: string;
+@Input() richCard: boolean;
+// @Input() jobTitle: string;
+// @Input() jobSubtitle: boolean;
+@Input() cards: string;
+@Input() company: string;
+@Input() title: string;
+@Input() description: string;
+@Input() baseSalary: any;
+@Input() salaryType: string;
+@Input() employmentType: string;
+@Input() logo: string;
+@Input() url: string;
+
 @Output() descriptionEmitter = new EventEmitter<string>();
 userId = JSON.parse(localStorage.getItem('user')).uid;
 
@@ -29,6 +46,40 @@ userId = JSON.parse(localStorage.getItem('user')).uid;
     ) { }
 
   ngOnInit(): void {
+  }
+
+  heartFilled: boolean = false;
+
+  bookmarkJob() {
+    this.heartFilled = true;
+    let data = {
+      description: this.description,
+      url: this.url,
+      logo: this.logo,
+      // jobImageURL: this.jobImageURL,
+      title: this.title,
+      company: this.company,
+      employmentType: this.employmentType,
+      salary: this.baseSalary,
+      //hourly, weekly, monthly, annually
+      salaryType: this.salaryType,
+
+
+    }
+
+    let ref = this.db.collection("bookmarks").doc(this.userId).collection("bookmarks")
+    ref.add(data);
+  }
+
+  //TODO: Code this functionality
+  learnMore() {
+    this.descriptionEmitter.emit(this.description)
+    console.log(this.description)
+  }
+
+  apply() {
+    window.open(this.applyNowUrl, "_blank")
+
   }
 
 }
